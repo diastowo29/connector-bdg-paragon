b2cb5906e001fd3aadb824522c09dae8cf4b9025
diastowo29 <diastowo@gmail.com>
2025-12-01 16:51:48 +0700

    update keset

--- config/encrypt/config.js
+++ config/encrypt/config.js
@@ -1,15 +1,15 @@
 const CryptoJS = require('crypto-js')
 require('dotenv').config();
-const kesetSecret = 'W3lcome123';
+const keset = process.env.KESET;
 
 function getDecryptedString (str){
-  const bytes = CryptoJS.AES.decrypt(str, kesetSecret)
+  const bytes = CryptoJS.AES.decrypt(str, keset)
   const decryptedData = bytes.toString(CryptoJS.enc.Utf8)
   return decryptedData;
 }
 
 function doEncryptString (str) {
-  const ciphertext = CryptoJS.AES.encrypt(str, kesetSecret).toString();
+  const ciphertext = CryptoJS.AES.encrypt(str, keset).toString();
   return ciphertext;
 }
 
